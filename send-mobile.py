7a573e3eecd8f410c133b5bc83566e82eaaa1b05
yangjinlong <510186869@qq.com>
2017-05-24 18:39:58 +0800

    python 2.7 以上版本均可使用

--- send-mobile.py
+++ send-mobile.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 #coding:utf-8
 
 import tornado.ioloop
@@ -6,7 +5,7 @@ import tornado.web
 import json
 import sys
 import urllib
-import urllib2
+#import urllib2
 import base64
 import time
 from  datetime import datetime
@@ -18,18 +17,19 @@ def send_message_example(content,tos):
     resp = requests.post(("http://hy6.nbark.com:7602/sms.aspx?action=send"),
                          data={
                              "userid":"174",
-                             "account":"XXXXXXXXXXXXXX",
-                             "password":"XXXXXXXXXXXXXXX",
+                             "account":"youzhongyxhy6",
+                             "password":"IXh9UT9KyhK8GcAZ0lJv",
                              "mobile":tos,
                              "content":content,
                              "type":"json"
                          },timeout=3,verify=False);
-    result = json.loads(resp.content)
+    result = resp.content
+
 
 ###获取post请求内容
 class add(tornado.web.RequestHandler):
     def post(self, content=None):
-        res = Add(json.loads(self.request.body))
+        res = Add(json.loads(self.request.body.decode()))
         self.write(json.dumps(res))
 
 
